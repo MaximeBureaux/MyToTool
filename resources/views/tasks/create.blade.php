cbdc300be49597ff4e71aa0fe4b06e846d1911d2
MaximeBureaux <maxime@digiworks.fr>
2022-05-24 23:18:25 +0200

    Ajout punaise + style create, edit & show

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,9 +5,9 @@
         </h2>
     </x-slot>    
 
-    <div class="py-12">
+    <div class="py-12 overflow-hidden">
       <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white">
-        <div class="w-full sm:max-w-lg mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <div class="w-full sm:max-w-lg mt-6 px-6 py-4 bg-white shadow-md sm:rounded-lg relative">
           <x-jet-validation-errors class="mb-4" />
           <form method="POST" action="{{ route('tasks.store') }}">
               @csrf
@@ -29,6 +29,25 @@
                   </x-jet-button>
               </div>
           </form>
+          <div class="absolute -right-10 -top-5">
+            <div class="relative bg-[#fffdd9] opacity-[0.9] w-32 h-12 rotate-[30deg]">
+                <div class="scotch absolute bg-[#fffdd9] w-10 h-12 -right-9 rotate-180"></div>
+                <div class="scotch absolute bg-[#fffdd9] w-10 h-12 -left-9 "></div>
+            </div>
+          </div>
+          <div class="absolute -bottom-3 -left-3">
+            <div class="relative w-fit">
+                <div class="gradiant-thumbtack-dot w-4 h-[3px] -rotate-45 absolute -right-1 top-2"></div>
+                  <div class="gradiant-thumbtack-base w-10 h-10 rounded-full relative">
+                    <div class="bg-white w-[2px] h-[7px] rounded-lg absolute top-2 right-2 -rotate-[35deg] opacity-70 z-10"></div>
+                    <div class="gradiant-thumbtack-middle w-6 h-6 rounded-full absolute top-3 right-3">
+                        <div class="gradiant-thumbtack-top w-7 h-7 rounded-full absolute top-2 right-2">
+                            <div class="bg-white w-[2px] h-[4px] rounded-lg absolute top-2 right-2 -rotate-[35deg] opacity-70"></div>
+                        </div>
+                    </div>
+                  </div>
+              </div>
+          </div>
         </div>
       </div>
     </div>
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -5,27 +5,49 @@
         </h2>
     </x-slot>    
 
-    <div class="py-12">
+    <div class="py-12 overflow-x-hidden min-h-[80vh]">
       <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white">
-        <div class="w-full sm:max-w-lg mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-          <p class="text-2xl">Titre</p>
-          <p>{{ $task->title }}</p>
-          <p class="text-2xl">Détail</p>
-          <p>{{ $task->detail }}</p>
-          <p class="text-2xl">Etat</p>
-          <p>
-            @if($task->state)
-              La tâche a été accomplie !
-            @else
-              La tâche n'a pas encore été accomplie.
-            @endif
-          </p>
-          <p class="text-2xl">Date de création</p>
-          <p>{{ $task->created_at->format('d/m/Y') }}</p>
+        <div class="w-full flex flex-col gap-5 sm:max-w-xl mt-6 px-6 py-4 bg-white shadow-md sm:rounded-lg relative">
+          <div>
+            <p class="text-2xl">Titre</p>
+            <p>{{ $task->title }}</p>
+          </div>
+          <div>
+            <p class="text-2xl">Détail</p>
+            <p>{{ $task->detail }}</p>
+          </div>
+          <div>
+            <p class="text-2xl">Etat</p>
+            <p>
+              @if($task->state)
+                La tâche a été accomplie !
+              @else
+                La tâche n'a pas encore été accomplie.
+              @endif
+            </p>
+          </div>
+          <div>
+            <p class="text-2xl">Date de création</p>
+            <p>{{ $task->created_at->format('d/m/Y') }}</p>
+          </div>
           @if($task->created_at != $task->updated_at)
-            <p class="text-2xl">Dernière mise à jour</p>
-            <p>{{ $task->updated_at->format('d/m/Y') }}</p>
+          <div>
+              <p class="text-2xl">Dernière mise à jour</p>
+              <p>{{ $task->updated_at->format('d/m/Y') }}</p>
+          </div>
           @endif
+          <div class="absolute -right-10 -top-5">
+            <div class="relative bg-[#fffdd9] opacity-[0.9] w-32 h-12 rotate-[30deg]">
+                <div class="scotch absolute bg-[#fffdd9] w-10 h-12 -right-9 rotate-180"></div>
+                <div class="scotch absolute bg-[#fffdd9] w-10 h-12 -left-9 "></div>
+            </div>
+          </div>
+          <div class="absolute -left-10 -bottom-10">
+            <div class="relative bg-[#fffdd9] opacity-[0.9] w-32 h-12 rotate-[30deg]">
+                <div class="scotch absolute bg-[#fffdd9] w-10 h-12 -right-9 rotate-180"></div>
+                <div class="scotch absolute bg-[#fffdd9] w-10 h-12 -left-9 "></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
